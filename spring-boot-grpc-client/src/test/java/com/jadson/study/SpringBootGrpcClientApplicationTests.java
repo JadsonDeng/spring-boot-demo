bc7d54c7c986f751849a78ebd0c74444e6da78df
dengjunzhen <aaa@bbb.com>
2022-07-17 23:07:32 +0800

    init

---
+++ spring-boot-grpc-client/src/test/java/com/jadson/study/SpringBootGrpcClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadson.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGrpcClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-boot-grpc-server/src/test/java/com/jadson/study/SpringBootGrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadson.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGrpcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
