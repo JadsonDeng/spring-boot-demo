007e53943f66978c32ec2cbbe52bd748833bf6f6
dengjunzhen <dengjunzhen@kuaishou.com>
2022-07-21 22:59:46 +0800

    update

--- spring-boot-grpc-server/src/main/java/com/jadson/study/SpringBootGrpcServerApplication.java
+++ spring-boot-grpc-server/src/main/java/com/jadson/study/SpringBootGrpcServerApplication.java
@@ -2,8 +2,10 @@ package com.jadson.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringBootGrpcServerApplication {
 
     public static void main(String[] args) {
